5ee82abe0b6ca7ef3588799361b93aef0a40d03d
Kez Anwar <kezanwar@gmail.com>
2024-08-15 09:15:41 +0100

    moved history email to own class

---
+++ email/history.ts
@@ -0,0 +1,22 @@
+import { readFile } from "fs/promises";
+
+class MailHistory {
+  static async getDontEmailList() {
+    try {
+      const history = await readFile(`${process.cwd()}/dont-email.json`);
+      const emails = JSON.parse(String(history)).emails;
+      return emails;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  static extractEmails(str: string) {
+    const email = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b/g;
+    return str.match(email);
+  }
+
+  static removeDuplicates(arr: any[] | null) {
+    return [...new Set(arr)];
+  }
+}
--- email/index.ts
+++ email/index.ts
@@ -32,25 +32,6 @@ class Email {
     });
   }
 
-  static async getDontEmailList() {
-    try {
-      const history = await readFile(`${process.cwd()}/dont-email.json`);
-      const emails = JSON.parse(String(history)).emails;
-      return emails;
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  static extractEmails(str: string) {
-    const email = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b/g;
-    return str.match(email);
-  }
-
-  static removeDuplicates(arr: any[] | null) {
-    return [...new Set(arr)];
-  }
-
   static async send(mailOptions: Mail.Options) {
     try {
       const info = await transporter.sendMail(mailOptions);
