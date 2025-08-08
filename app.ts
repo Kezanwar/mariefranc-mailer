a1fc7d1f1ad7d6f7f31cdd6c48a2f6de33edbc4e
Kez Anwar <kezanwar@gmail.com>
2025-08-08 22:49:18 +0100

    [LTLO EPK] sending travs outreach email with Leave The Light On EPK

--- app.ts
+++ app.ts
@@ -1,35 +1,72 @@
 import EmailService from "./email";
-import ExcelService, { Row } from "./excel";
+import { emailAddresses, testEmails } from "./travs-emails";
+import fs from "fs/promises";
 
-const venueData = ExcelService.getVenueDataToEmail();
+const BATCH_AMOUNT = 10;
+const END = testEmails.length;
 
-const rows: Row[] = venueData[0].data;
+let last_index = 0;
+let batch_count_since_timeout = 0;
+let successful_email_count = 0;
+let rejected_email_count = 0;
 
-for await (let [index, row] of rows.entries()) {
-  if (index === 0) {
-    //* this is the column names row, no action needed
-    continue;
-  }
+console.log(
+  `😁 Bot Started - emailing ${emailAddresses.length} email addresses with Travs LTLO EPK Email`
+);
 
-  if (row[ExcelService.cols["Has Been Emailed"]] !== "Yes") {
-    const emails = row[ExcelService.cols["Venue Email"]];
-    const venueName = row[ExcelService.cols["Venue Name"]];
+while (true) {
+  const batch = testEmails.slice(last_index, last_index + BATCH_AMOUNT);
 
-    if (emails) {
-      for await (let email of emails.split(";")) {
-        const to = {
-          receiver: venueName,
-          to: email,
-        };
+  const promises = batch.map((email) => EmailService.sendTravsEmail(email));
 
-        await EmailService.sendAvailabilityTemplate(to); //TODO: should catch and handle an email error here.
+  const sendBatch = await Promise.allSettled(promises);
 
-        row[ExcelService.cols["Has Been Emailed"]] = "Yes";
-      }
+  const batch_successful_emails: string[] = [];
+
+  for (let [index, result] of sendBatch.entries()) {
+    if (result.status === "rejected") {
+      console.error(`❌ Error sending email to: ${batch[index]}`);
+      rejected_email_count++;
+    } else {
+      console.log(`✅ Successfully emailed ${batch[index]}`);
+      successful_email_count++;
+      batch_successful_emails.push(batch[index]);
     }
   }
 
-  const newSheet = ExcelService.buildSheet(venueData);
+  last_index = last_index + BATCH_AMOUNT;
 
-  ExcelService.createFile("send-email-venue-data.xlsx", newSheet);
+  let existing_succesful_emails: string[] = [];
+
+  const data = await fs.readFile("travs_sent.json", "utf-8");
+
+  if (data) {
+    existing_succesful_emails = JSON.parse(data);
+  }
+
+  const allSentEmails = [
+    ...existing_succesful_emails,
+    ...batch_successful_emails,
+  ];
+
+  await fs.writeFile("travs_sent.json", JSON.stringify(allSentEmails, null, 2));
+
+  console.log(`🧮 Current successful email total: ${successful_email_count}`);
+  console.log(`🧮 Current rejected email total: ${rejected_email_count}`);
+  console.log("🧑🏾‍💻 Updated sent email doc");
+
+  if (last_index >= END) {
+    console.log("🚀 Finished sending all emails");
+    break;
+  }
+
+  if (batch_count_since_timeout === 3) {
+    console.log(
+      "😴 Sent 40 emails in the last 60 seconds - Sleeping for 45 seconds to not overload the Gmail API and get flagged for spam"
+    );
+    await EmailService.fakeLongLoadPromise(45000);
+    batch_count_since_timeout = 0;
+  } else {
+    batch_count_since_timeout++;
+  }
 }
--- email/index.ts
+++ email/index.ts
@@ -76,6 +76,32 @@ class EmailService {
     await this.fakeLongLoadPromise();
   }
 
+  static async sendTravsEmail(to: string) {
+    return this.send({
+      to: to,
+      from: process.env.GMAIL_USER,
+      subject: "Marie Franc New Single - Leave The Light On",
+      html: `
+        <div style="font-family: Arial, sans-serif; line-height: 1.6; color: #333;">
+          <p>Hello!</p>
+          
+          <p>I'm excited to share the press release for Marie Franc's upcoming single, 'Leave The Light On', set for release on 22nd August.</p>
+          
+          <p>This track is the band's second single of the year and the second release from their debut EP Saturday Boy, following an early spin on BBC Introducing Manchester. 'Leave The Light On' blends dream pop and soul-tinged indie, with sultry guitar textures and emotionally rich vocals that create an intimate, evocative sound.</p>
+          
+          <p>Marie Franc is gaining momentum, with a sold-out headline show and a main stage performance at Moovin Festival alongside Ezra Collective. More live dates are coming this autumn.</p>
+          
+          <p>Click here to listen to the preview :)</p>
+          
+          <p>We've put together a <a href="https://drive.google.com/drive/folders/17o5EA-gr6yWVCR1GA_lYim-DX1UgJqCd?usp=sharing" target="_blank">Google Drive</a> with press assets, including the music video, press release, and other materials. We'd love to collaborate on any press opportunities!</p>
+          
+          <p>Many thanks,<br>
+          Bonnie</p>
+        </div>
+      `,
+    });
+  }
+
   static printSting(str: string) {
     console.log(str);
   }
